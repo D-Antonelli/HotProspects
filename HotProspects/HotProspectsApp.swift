019f39afa10d33e9c2aa301551643f3e8dd5c3a6
derya <derya@boonandmoil.dev>
2023-02-06 10:43:25 +0000

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Derya Antonelli on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
